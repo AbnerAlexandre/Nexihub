a9068d26c636d5bc13964dc1ff9a8c716cd7ed1c
AbnerAlexandre <abneralecspb@hotmail.com>
2025-11-11 23:24:51 -0300

    feat(database): centraliza exportações de schemas
    - Exporta todos os schemas, relations e validações
    - Facilita imports em outros módulos
    - Organização modular do banco de dados

---
+++ api/src/db/schema/index.ts
@@ -0,0 +1,41 @@
+export * from './enum'
+
+// Membros
+export * from './membros/membros'
+export * from './membros/membros.relations'
+export * from './membros/membros.schemas'
+
+// Intenções de Participação
+export * from './intencoes-participacao/intencoes-participacao'
+export * from './intencoes-participacao/intencoes-participacao.relations'
+export * from './intencoes-participacao/intencoes-participacao.schemas'
+
+// Indicações
+export * from './indicacoes/indicacoes'
+export * from './indicacoes/indicacoes.relations'
+export * from './indicacoes/indicacoes.schemas'
+
+// Obrigados
+export * from './obrigados/obrigados'
+export * from './obrigados/obrigados.relations'
+export * from './obrigados/obrigados.schemas'
+
+// Reuniões
+export * from './reunioes/reunioes'
+export * from './reunioes/reunioes.relations'
+export * from './reunioes/reunioes.schemas'
+
+// Reuniões-Membros
+export * from './reunioes-membros/reunioes-membros'
+export * from './reunioes-membros/reunioes-membros.relations'
+export * from './reunioes-membros/reunioes-membros.schemas'
+
+// Presenças
+export * from './presencas/presencas'
+export * from './presencas/presencas.relations'
+export * from './presencas/presencas.schemas'
+
+// Mensalidades
+export * from './mensalidades/mensalidades'
+export * from './mensalidades/mensalidades.relations'
+export * from './mensalidades/mensalidades.schemas'
