4f812d077c500b95dd618c9d9cf6bc9c1a2cd219
AbnerAlexandre <abneralecspb@hotmail.com>
2025-11-12 20:18:26 -0300

    feat(web): configuração estilos e tokens de cor

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,10 @@
 
+import { Header }  from "@/components/common";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Olá
+    <div>
+      <Header />
     </div>
   );
 }
